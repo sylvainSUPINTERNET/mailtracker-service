bf453793ca85b4d840be70243e88f33e432df31b
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2023-11-20 23:59:40 +0100

    [XD]

--- src/controllers/pixel.controller.ts
+++ src/controllers/pixel.controller.ts
@@ -1,5 +1,13 @@
 export async function pixelController (fastify:any, options:any) {
-    fastify.get('/', async (request:any, reply:any) => {
-      return { hello: 'world' }
-    })
+    // fastify.get('/', async (request:any, reply:any) => {
+    //   return { hello: 'world' }
+    // });
+
+    fastify.get('/pixel', function (req:any, reply:any) {
+      if ( req.query ) {
+        console.log("pixel id: ", req.query);
+      }
+      console.log(req);
+      reply.sendFile('1x1.png', { cacheControl: false }) // overriding the options disabling cache-control headers
+    });
 }
--- src/index.ts
+++ src/index.ts
@@ -25,17 +25,6 @@ fastify.register(require('@fastify/static'), {
   // constraints: { host: 'example.com' } // optional: default {}
 })
 
-fastify.get('/pixel', function (req, reply:any) {
-
-  if ( req.query ) {
-    console.log("pixel id: ", req.query);
-  }
-
-  console.log(req);
-
-  reply.sendFile('1x1.png', { cacheControl: false }) // overriding the options disabling cache-control headers
-});
-
 fastify.register(pixelController);
 
 
