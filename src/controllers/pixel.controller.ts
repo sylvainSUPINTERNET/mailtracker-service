a2de32acd75abd370d909b25809c68613a5ae7f4
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2023-11-21 23:05:01 +0100

    wip

--- src/controllers/pixel.controller.ts
+++ src/controllers/pixel.controller.ts
@@ -1,13 +1,15 @@
-export async function pixelController (fastify:any, options:any) {
-    // fastify.get('/', async (request:any, reply:any) => {
-    //   return { hello: 'world' }
-    // });
+import { isTrackable } from "../services/track.service";
 
-    fastify.get('/pixel', function (req:any, reply:any) {
-      if ( req.query ) {
-        console.log("pixel id: ", req.query);
-      }
-      console.log(req);
-      reply.sendFile('1x1.png', { cacheControl: false }) // overriding the options disabling cache-control headers
-    });
+export async function pixelController (fastify:any, options:any) {
+  
+  fastify.get('/:uuid/pixel', async function (req:any, reply:any) {
+
+    const userIsTrackable:boolean|null = await isTrackable(req);
+    if ( userIsTrackable ) {
+      console.log("TRACK")
+    }
+
+    return reply.sendFile('1x1.png', { cacheControl: false }) // overriding the options disabling cache-control headers
+
+  });
 }
