aafaef49252f98d4f530584647aa760eb1e17323
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2023-11-23 21:36:10 +0100

    [UPDATE]

--- src/controllers/pixel.controller.ts
+++ src/controllers/pixel.controller.ts
@@ -12,4 +12,18 @@ export async function pixelController (fastify:any, options:any) {
     return reply.sendFile('1x1.png', { cacheControl: false }) // overriding the options disabling cache-control headers
 
   });
+
+
+
+  fastify.get('/:uuid/pixel/:encodedUriPixelName', async function (req:any, reply:any) {
+
+    const userIsTrackable:boolean|null = await isTrackable(req);
+
+    if ( userIsTrackable && req.params.encodedUriPixelName ) {
+      console.log("Notify user " + req.params.uuid + " about " + req.params.encodedUriPixelName, " get opened");
+    }
+    
+    return reply.sendFile('1x1.png', { cacheControl: false }) // overriding the options disabling cache-control headers
+
+  });
 }
