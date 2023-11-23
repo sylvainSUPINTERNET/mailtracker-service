76b926b5a96e9603faf426500a2f6a19c68f4045
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2023-11-23 21:37:08 +0100

    [UPDATE]

--- src/controllers/pixel.controller.ts
+++ src/controllers/pixel.controller.ts
@@ -2,19 +2,6 @@ import { isTrackable } from "../services/track.service";
 
 export async function pixelController (fastify:any, options:any) {
   
-  fastify.get('/:uuid/pixel', async function (req:any, reply:any) {
-
-    const userIsTrackable:boolean|null = await isTrackable(req);
-    if ( userIsTrackable ) {
-      console.log("TRACK")
-    }
-
-    return reply.sendFile('1x1.png', { cacheControl: false }) // overriding the options disabling cache-control headers
-
-  });
-
-
-
   fastify.get('/:uuid/pixel/:encodedUriPixelName', async function (req:any, reply:any) {
 
     const userIsTrackable:boolean|null = await isTrackable(req);
